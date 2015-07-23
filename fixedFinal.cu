1ac8ce52502730c56efca8668bf79f116904b647
marcelacrosariol <marcela@crosariol.com.br>
2015-07-23 14:59:02 -0400

    fixed halo functions

--- fixedFinal.cu
+++ fixedFinal.cu
@@ -5,7 +5,7 @@
 
 #define RANDVAL 1984
 #define BLOCK_SIZE 16
-#define DIM     8 // Linear dimension of our grid - not counting ghost cells
+#define DIM     4 // Linear dimension of our grid - not counting ghost cells
 
 // Create an array that stores the number of rows of the subGrid in each device
 __host__ void RowCount(int devCount,int *subGridSize){
@@ -38,33 +38,33 @@ __host__ int getLastRow(int device, int *subGridSize){
         return subGridSize[device] + getLastRow(device-1, subGridSize);
 }
 
-__host__ void buildHaloRows(int *h_subgrid, int *h_grid, int firstRow, int lastRow){
-    for (int j = 1; j <= DIM; j++){
-        if (firstRow == 1){ // If the subGrid contains the first row of the main grid
-            // Copy last real row to the top halo/ghost row
-            h_subgrid[j] = h_grid[DIM * (DIM+2) + j];
-            // Copy the botton halo row from the main grid. Probably it's not the last row on the main grid, so it'll get the row after the last row of the subGrid
-            h_subgrid[(lastRow+1) * (DIM+2) + j] = h_grid[(lastRow+1) * (DIM+2) + j];
-        }
-        else if(lastRow == DIM){ // If the subGrid contains the last row of the main grid
-            // Copy the first real row to the bottom halo/ghost row
-            h_subgrid[(DIM+1) * (DIM+2) + j] = h_grid[(DIM+2) + j];
-            // Copy top halo row. Copy the row before the fsrst row in the subGrid
-            h_subgrid[(firstRow-1) * (DIM+2) + j] = h_grid[(firstRow-1) * (DIM+2) + j];
-        }
-        else{
-            h_subgrid[(firstRow-1) * (DIM+2) + j] = h_grid[(firstRow-1) * (DIM+2) + j];
-            h_subgrid[(lastRow+1) * (DIM+2) + j] = h_grid[(lastRow+1) * (DIM+2) +j];
-        }
+__host__ void buildHaloCells(int *h_grid){
+    // Copy halo rows
+    for (int j = 1; j<= DIM; j++){
+
+        // Copy first real row to last halo row
+        h_grid[(DIM+1) * (DIM+2) + j] = h_grid[1 * (DIM+2) + j];
+
+        // Copy last real row to first halo row
+        h_grid[j] = h_grid[DIM * (DIM+2) + j];
+    }
+
+    // Copy halo columns
+    for (int i = 0; i<= DIM+1; i++){
+
+        // Copy first real column to last halo column
+        h_grid[i * (DIM+2) + (DIM+1)] = h_grid[i* (DIM+2) + 1];
+
+        // Copy last real column to first halo column
+        h_grid[i * (DIM+2)] = h_grid[i * (DIM+2) + DIM];
     }
 }
 
-__host__ void buildHaloCols(int *h_subgrid, int *h_grid, int firstRow, int lastRow){
-    for(int i = firstRow-1; i <= lastRow+1; i++){
-        // Copy the last real cell from the row to the left halo cell
-        h_subgrid[i*(DIM+2)] = h_grid[i*(DIM+2) + DIM];
-        // Copy the first real column cell to the right halo cell
-        h_subgrid[i * (DIM+2) + (DIM+1)] = h_grid[i * (DIM+2) + 1];
+__host__ void buildSubGrid(int *h_grid, int *h_subGrid, int firstRow, int lastRow, int d){
+    for(int i = firstRow-1; i<= lastRow+1; i++){
+        for(int j = 0; j<= DIM+1; j++){
+            h_subGrid[i * (DIM+2) + j] = h_grid[i * (DIM+2) + j];
+        }
     }
 }
 
@@ -76,6 +76,7 @@ __global__ void ghostRows(int *grid){
     if (id <= DIM){
         //Copy first real row to bottom ghost row
         grid[(DIM+2)*(DIM+1)+id] = grid[(DIM+2)+id];
+
         //Copy last real row to top ghost row
         grid[id] = grid[(DIM+2)*DIM + id];
     }
@@ -88,6 +89,7 @@ __global__ void ghostCols(int *grid){
     if (id <= DIM+1){
         //Copy first real column to right most ghost column
         grid[id*(DIM+2)+DIM+1] = grid[id*(DIM+2)+1];
+
         //Copy last real column to left most ghost column
         grid[id*(DIM+2)] = grid[id*(DIM+2) + DIM];
     }
@@ -112,7 +114,7 @@ __global__ void GOL(int *grid, int *newGrid, int firstRow){
 
         int cell = grid[id];
 
-        printf("firstrow: %d ID: %d Grid[%d]: %d cell: %d Neighboors: %d \n", firstRow, id, id, grid[id], cell ,numNeighbors);
+        //printf("firstrow: %d ID: %d Grid[%d]: %d cell: %d Neighboors: %d \n", firstRow, id, id, grid[id], cell ,numNeighbors);
 
         // Here we have explicitly all of the game rules
         if (cell == 1 && numNeighbors < 2)
@@ -151,14 +153,22 @@ int main(int argc, char* argv[]){
     
     srand(RANDVAL);
     // Assign random value to cells of the grid
-    //#pragma omp parallel for private(i,j)
+    #pragma omp parallel for private(i,j)
     for(i = 1; i<=DIM; i++) {
         for(j = 1; j<=DIM; j++) {
            h_grid[i*(DIM+2)+j] = rand() % 2;
-           printf("%d  ", h_grid[i*(DIM+2)+j]);
         }
-        printf("\n");
     } // End of pragma
+
+
+printf("\n");
+for(int i = 1; i <= DIM; i++){
+for(int j = 1; j <= DIM; j++){
+printf("%d  ", h_grid[i*(DIM+2)+j]);
+}
+printf("\n");
+}
+
 
     dim3 blockSize(BLOCK_SIZE, BLOCK_SIZE,1);
     int  linGrid = (int)ceil(DIM/(float)BLOCK_SIZE);
@@ -222,8 +232,18 @@ int main(int argc, char* argv[]){
         h_SubGridSize = (int*)malloc(sizeof(int)*devCount); // Allocate memory for the subGridSize, which stores the number of elements in each subGrids
         RowCount(devCount, h_SubGridSize); // Calculate the size of the subgrid in each GPU
 
+        buildHaloCells(h_grid);
+
+        printf("\n");
+        for(int i = 0; i <= DIM+1; i++){
+            for(int j = 0; j <= DIM+1; j++){
+                printf("%d  ", h_grid[i*(DIM+2)+j]);
+            }
+        printf("\n");
+        }
+
         omp_set_num_threads(devCount);
-        
+
         for (iter = 0; iter < lim; iter ++){
 
             #pragma omp parallel private(currentDevice, CPUthreadId, h_subGrid, subBytes, firstRow, lastRow)
@@ -245,33 +265,25 @@ int main(int argc, char* argv[]){
                 // Calculates the last row of the submatrix in the main matrix
                 lastRow = getLastRow(currentDevice, h_SubGridSize);
 
-                for(int i = firstRow; i <= lastRow; i++){
-                    for(int j = 1; j <= DIM; j++){
-                        h_subGrid[i*(DIM+2)+j] = h_grid[i*(DIM+2)+j];
-
-                    }
-                }
+                buildSubGrid(h_grid, h_subGrid, firstRow, lastRow, currentDevice);
 
                 if(iter == 0){
                     printf("\n printing subgrid gpu %d  \n", currentDevice);
                     printf("firstrow: %d lastRow:%d \n", firstRow, lastRow);
-                    for(int i = firstRow; i <= lastRow; i++){
-                        for(int j = 1; j <= DIM; j++){
+                    for(int i = firstRow-1; i <= lastRow+1; i++){
+                        for(int j = 0; j <= DIM+1; j++){
                             printf("%d  ", h_subGrid[i*(DIM+2)+j]);
                         }
-                            printf("\n");
+                        printf("\n");
                     }
                 }
 
-                buildHaloRows(h_subGrid, h_grid, firstRow, lastRow);
-                buildHaloCols(h_subGrid, h_grid, firstRow, lastRow);
 
                 cudaMemcpy(d_subGrid, h_subGrid, subBytes, cudaMemcpyHostToDevice);
 
                 // call GOL function and the new values will go to the d_tempSub grid
                 GOL<<<gridSize, blockSize>>>(d_subGrid, d_tempSub, firstRow);
 
-                free(h_subGrid);
                 h_subGrid = (int*)malloc(subBytes); //allocate memory for the subGrid
 
                 cudaMemcpy(h_subGrid, d_tempSub, subBytes, cudaMemcpyDeviceToHost);
